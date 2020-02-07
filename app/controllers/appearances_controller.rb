5e3118767994b19eba934e2edbca719e51080d0e
github username <github email address>
2020-02-07 11:12:37 -0500

    standard deliverables

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -3,13 +3,19 @@ class AppearancesController < ApplicationController
         @app = Appearance.new
         @guests = Guest.all
         @episodes = Episode.all
+        @errors = flash[:errors]
     end
     def create
         @app = Appearance.new(app_params(:guest_id, :episode_id, :rating))
-        @app.save
-        ep_id = @app.episode_id
-        @ep = Episode.find_by(id: ep_id)
-        redirect_to episode_path(@ep)
+        if @app.valid?
+            @app.save
+            ep_id = @app.episode_id
+            @ep = Episode.find_by(id: ep_id)
+            redirect_to episode_path(@ep)
+        else
+            flash[:errors] = @app.errors.full_messages
+            redirect_to new_appearance_path
+        end
     end
     private
     def app_params(*args)
--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -1,4 +1,7 @@
 class Appearance < ApplicationRecord
     belongs_to :guest
     belongs_to :episode
+
+    validates :rating, numericality: {greater_than_or_equal_to: 1, message: "error: The appearance rating cannot be less than 1"}
+    validates :rating, numericality: {less_than_or_equal_to: 5, message: "error: The appearance rating cannot be more than 5"}
 end
