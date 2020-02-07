120c1a9531e3aed7db400137a28b34caa6c36c9a
Steven Zhu <stevenzhu03@gmail.com>
2020-02-07 11:38:41 -0500

    Completed Min

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,2 +1,13 @@
 class Episode < ApplicationRecord
+    has_many :appearances
+    has_many :guests, through: :appearances
+
+    # def average_rating
+    #     app_total = 0
+    #         self.appearaneces.each do |appearance|
+    #             app_total += appearance.rating
+    #         end
+        
+    #     return app_total / self.appearances.count 
+    # end
 end
