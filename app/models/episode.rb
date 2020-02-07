9fa76412e6b0c8804f0392205dec6726b64b80f3
Granit <ghaxhaj@fordham.edu>
2020-02-07 11:41:42 -0500

    Frick

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,2 +1,11 @@
 class Episode < ApplicationRecord
+    has_many :appearances
+    has_many :guests, through: :appearances
+
+    def average_rating
+        a = self.appearances.inject(0){|sum,a| sum + a.rating}.to_f
+        b= self.appearances.count
+        c = a/b
+        c
+    end
 end
