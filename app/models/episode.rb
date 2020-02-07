adcbc2beb8f959db1214bbfd68e23056b58bfa0c
Nathan Brickett <natey37@gmail.com>
2020-02-07 11:41:59 -0500

    wowoow

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,2 +1,17 @@
 class Episode < ApplicationRecord
+
+    has_many :appearances
+    has_many :guests, through: :appearances
+
+
+    def avg_rating
+        if self.appearances.count != 0
+            self.appearances.map{|appearance| appearance.rating}.inject(:+) / self.appearances.count
+        end 
+    end
+
+    # def sorted  
+    #     self.appearances.episodes.sort_by{|episode| episode.rating}
+    # end  
+    
 end
